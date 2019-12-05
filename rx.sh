19c35808fc5066bfb0cfb6c2f3ba42eb95b7be88
nobody246 <a.se8seven@gmail.com>
2019-12-05 02:12:03 -0500

    Made bursts faster

--- rx.sh
+++ rx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-play burstCW.wav speed $1 #tempo $1 10 10 10
-sox -t wav burstCW.wav -t wav burstCWSlowed$1.wav speed $1 #tempo $1 10 10 10
+sox -t wav burstCW.wav -t wav /tmp/burstCW1.wav speed .11
+sox -t wav /tmp/burstCW1.wav -t wav burstCWSlowed$1.wav speed .11 #tempo $1 10 10 10
