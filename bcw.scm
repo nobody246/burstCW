70da85c05eaf13949307f8f68de7ed0ff20f8e6f
nobody246 <a.se8seven@gmail.com>
2019-12-04 23:05:47 -0500

    Added files

---
+++ bcw.scm
@@ -0,0 +1,104 @@
+(use sndfile srfi-14 loops)
+(define *pi_2  (* 2 (acos -1.0)))
+(define alpha (string->list "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "))
+(define alpha-codes '(".-"
+                      "-..."
+                      "-.-."
+                      "-.."
+                      " . "
+                      "..-."
+                      "--."
+                      "...."
+                      " .. "
+                      ".---"
+                      "-.-"
+                      ".-.."
+                      "--"
+                      "-."
+                      "---"
+                      ".--."
+                      "--.-"
+                      ".-."
+                      "..."
+                      "-"
+                      "..-"
+                      "...-"
+                      ".--"
+                      "-..-"
+                      "-.--"
+                      "--.."
+                      "-----"
+                      ".----"
+                      "..---"
+                      "...--"
+                      "....-"
+                      "....."
+                      "-...."
+                      "--..."
+                      "---.."
+                      "----."
+                      " "))
+(define file-copy "burstCW.wav")
+(define s-per-sec 11025)
+(define chans 1)
+(define freq 2600)
+(define volume 1.0)
+(define millisecond (/ s-per-sec 1000))
+(define sp-count 250) 
+(define dot-count 60)
+(define dash-count 240)
+(define inter-char 150)
+(define msg (string->list (cadr (command-line-arguments))))
+(print "msg:" msg)
+(define fr-cnt 0)
+(define (ch l k #!optional (i 0))
+  (if (eq? (car l) k)
+      i
+      (if (null? l)
+          #f
+          (ch (cdr l) k (add1 i)))))
+(define (get-char-wav-seq wr h x)
+  (let ((y '()))
+    (when (not (null? x))
+      (set! y (car x)))
+    (cond
+     ((eq? y #\space)
+      (do-times i sp-count
+        (begin
+          (set! fr-cnt (add1 fr-cnt))
+          (wr h (list->f32vector '(0.0))))))
+     ((or (eq? y #\.) (eq? y #\-))
+      (let ((d (if (eq? y #\.) dot-count dash-count)))
+        (do-times i d
+          (begin
+            (set! fr-cnt (add1 fr-cnt))
+            (wr h (list->f32vector
+                   `(,(* volume (sin (/ (* *pi_2
+                                           freq
+                                           fr-cnt)
+                                        s-per-sec)))))))))))
+      (do-times i inter-char
+            (wr h (list->f32vector '(0.0)))))
+  (when (>= (length x) 1)
+      (get-char-wav-seq wr h (cdr x))))
+(define (make-wav wr h m)
+  (let ((x (if (null? m) (ch alpha #\space) (ch alpha (car m)))))
+      (get-char-wav-seq
+       wr
+       h
+       (string->list (list-ref alpha-codes
+                               x))))
+  (do-times i sp-count
+            (wr h (list->f32vector '(0.0))))
+  (when (not (null? m))
+    (make-wav wr h (if (null? m) m (cdr m)))))
+(with-sound-to-file
+ file-copy
+ '(wav pcm-16 file)
+ s-per-sec
+ chans
+ (lambda (h)
+   (make-wav write-items/f32
+             h
+             msg)))
+(exit)
---
+++ rx.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+play burstCW.wav tempo $1 10 10 10
