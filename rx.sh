ac08c110f1ffde32c0516c7a372ac36ab31c9bd5
nobody246 <a.se8seven@gmail.com>
2020-03-02 00:48:21 -0500

    Wrote my own

--- rx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sox -t wav burstCW.wav -t wav /tmp/burstCW1.wav speed .1
-sox -t wav /tmp/burstCW1.wav -t wav /tmp/burstCW2.wav speed .1
-sox -t wav /tmp/burstCW2.wav -t wav burstCWSlowed.wav speed .75
-rm /tmp/burstCW*wav
