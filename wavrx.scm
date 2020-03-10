c2b2ffcd2445c5feaa1ece90e7593c5ded88df6e
nobody246 <a.se8seven@gmail.com>
2020-03-10 16:30:02 -0400

    Changed incorrect wav slowdown method

--- wavrx.scm
+++ wavrx.scm
@@ -3,7 +3,7 @@
 (define file-out "burstCWSlowed.wav")
 (define slowdown-x (string->number (car (reverse (command-line-arguments)))))
 (when (not slowdown-x)
-  (set! slowdown-x 80))
+  (set! slowdown-x 1))
 (define f (file-open file-in
                      (+ open/read open/binary)))
 (define fo (file-open file-out
@@ -24,39 +24,28 @@
    (fmt-chunk-sample-depth 16 little unsigned)
    (data-chunk-id 32 bitstring)
    (data-chunk-size 32 little unsigned))
-   (file-write
-    fo
-    (bitstring->blob
-     (bitconstruct
-      (chunk-id 32  bitstring)
-      (chunk-size 32 little unsigned)
-      (riff-type  bitstring)
-      (fmt-chunk-id  bitstring)
-      (fmt-chunk-size 32 little unsigned)
-      (fmt-chunk-compression-type 16 little unsigned)
-      (fmt-chunk-channels 16 little unsigned)
-      (fmt-chunk-slice-rate 32 little unsigned)
-      (fmt-chunk-data-rate 32 little unsigned)
-      (fmt-chunk-block-alignment 16 little unsigned)
-      (fmt-chunk-sample-depth 16 little unsigned)
-      (data-chunk-id  bitstring)
-      ((* data-chunk-size slowdown-x)
-       32 little unsigned))))))
-(define z-cnt 0)
-(define f-sz (file-size f))
-(define (wr fo a #!optional (x 0))
-  (when (< x slowdown-x)
-    (file-write fo a)
-    (wr fo a (add1 x))))
-(define (r bt #!optional (cnt 1))
-  (let ((a (car bt)))
-    (wr fo a)
-    (and-let* ((bt (file-read f 1))
-               (iterate-again
-                (and (> f-sz cnt)
-                     (> (cadr bt) 0))))
-      (r bt (add1 cnt)))))
-(r (file-read f 1))
+  (let ((new-slice-rate (inexact->exact (round (/ fmt-chunk-slice-rate slowdown-x)))))
+     (print "Slowing down " fmt-chunk-slice-rate " by "
+            slowdown-x " times, new rate is " new-slice-rate)
+     (file-write
+      fo
+      (bitstring->blob
+       (bitconstruct
+        (chunk-id 32  bitstring)
+        (chunk-size 32 little unsigned)
+        (riff-type  bitstring)
+        (fmt-chunk-id  bitstring)
+        (fmt-chunk-size 32 little unsigned)
+        (fmt-chunk-compression-type 16 little unsigned)
+        (fmt-chunk-channels 16 little unsigned)
+        (new-slice-rate 32 little unsigned)
+        (fmt-chunk-data-rate 32 little unsigned)
+        (fmt-chunk-block-alignment 16 little unsigned)
+        (fmt-chunk-sample-depth 16 little unsigned)
+        (data-chunk-id  bitstring)
+        (data-chunk-size
+         32 little unsigned)))))))
+(file-write fo (car (file-read f (- (file-size f) 44))))
 (file-close fo)
 (file-close f)
 (exit)
