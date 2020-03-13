07778ef9168819f322c163a5eeceb8bfb67bbf58
nobody246 <a.se8seven@gmail.com>
2020-03-13 02:25:28 -0400

    check for invalid characters

--- bcw.scm
+++ bcw.scm
@@ -48,17 +48,16 @@
 (define interchar-count 15)
 (define cli (command-line-arguments))
 (define msg (map char-upcase (string->list (cadr cli))))
-(print "??" (length cli))
 (define freq (if (>= (length cli) 3) (string->number (caddr cli)) #f))
 (when (not freq)
   (set! freq 3600))
 (print "msg:" msg)
 (define fr-count 0)
 (define (ch l k #!optional (i 0))
-  (if (eq? (car l) k)
-      i
-      (if (null? l)
-          #f
+  (if (null? l)
+      #f
+      (if (eq? (car l) k)
+          i
           (ch (cdr l) k (add1 i)))))
 (define (get-char-wav-seq wr h x)
   (let ((y '()))
@@ -83,22 +82,23 @@
   (when (>= (length x) 1)
       (get-char-wav-seq wr h (cdr x))))
 (define (make-wav wr h m)
-  (let ((x (if (null? m) (ch alpha #\space) (ch alpha (car m)))))
+  (and-let* ((x (if (null? m) (ch alpha #\space) (ch alpha (car m)))))
       (get-char-wav-seq
        wr
        h
-       (string->list (list-ref alpha-codes x))))
-  (do-times i sp-count
-            (wr h (list->f32vector '(0.0))))
-  (when (not (null? m))
-    (make-wav wr h (if (null? m) m (cdr m)))))
+       (string->list (list-ref alpha-codes x)))
+      (do-times i sp-count
+                (wr h (list->f32vector '(0.0))))
+      (when (not (null? m))
+        (make-wav wr h (if (null? m) m (cdr m))))))
 (with-sound-to-file
  out-file
  '(wav pcm-16 file)
  s-per-sec
  chans
  (lambda (h)
-   (make-wav write-items/f32
-             h
-             msg)))
+   (when (not (make-wav write-items/f32
+                        h
+                        msg))
+     (print "undefined character found, valid characters are a-z,A-Z,0-9"))))
 (exit)
